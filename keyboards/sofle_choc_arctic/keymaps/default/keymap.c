6a626892fe99777e45e53a03f824ecff7c98de25
Johannes <johannes@macbook-jf.local>
2024-08-08 09:33:23 +0200

    minor keymap update

--- keyboards/sofle_choc_arctic/keymaps/default/keymap.c
+++ keyboards/sofle_choc_arctic/keymaps/default/keymap.c
@@ -40,14 +40,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                         KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_SLSH,
         KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                         KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_RSFT,
         KC_LCTL, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,   KC_MUTE,    KC_MPLY,  KC_N   , KC_M   , KC_COMM, KC_DOT , KC_QUOT, KC_RCTL,
-          KC_ESC , MO(2)  , KC_LALT, LT(1,KC_ENT), KC_LGUI,                     KC_RGUI, LT(1,KC_SPC), RALT_T(KC_BSPC), MO(2)   , KC_ESC),
+          KC_ESC , MO(2)  , KC_LGUI, LT(1,KC_ENT), KC_LALT,                    RALT_T(KC_BSPC), LT(1,KC_SPC),  KC_RGUI , MO(2)   , KC_ESC),
 
 	[1] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                      _______, KC_7   , KC_8   , KC_9   , LSFT(KC_EQL) , KC_MINS ,
-        _______, _______, _______, KC_LBRC, KC_RBRC, _______,                      _______, KC_4   , KC_5   , KC_6   , LSFT(KC_8), KC_SLSH,
-        _______, _______, _______, KC_LPRN, KC_RPRN, _______,                      KC_0   , KC_1   , KC_2   , KC_3   , KC_COMM, KC_EQL,
+        _______, _______, _______, _______, _______, _______,                      _______, KC_7   , KC_8   , KC_9   , KC_SLSH, KC_COMM ,
+        _______, _______, _______, KC_LBRC, KC_RBRC, _______,                      _______, KC_4   , KC_5   , KC_6   , LSFT(KC_8), KC_EQL,
+        _______, _______, _______, KC_LPRN, KC_RPRN, _______,                      KC_0   , KC_1   , KC_2   , KC_3   ,  KC_MINS , LSFT(KC_EQL) ,
         _______, _______, _______, KC_LCBR, KC_RCBR, _______, _______,    _______, KC_HOME, LALT(KC_LEFT), KC_UP, LALT(KC_RIGHT), KC_END, KC_PSCR,
-             _______, _______, _______, _______, _______,                             KC_DEL , _______, KC_LEFT, KC_DOWN, KC_RGHT),
+             _______, _______, _______, _______, _______,                             _______ , _______, KC_LEFT, KC_DOWN, KC_RGHT),
 	[2] = LAYOUT(
         _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                    KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11  ,
         _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, KC_F12  ,
