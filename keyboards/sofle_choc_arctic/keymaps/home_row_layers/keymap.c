25413b5503b477300e6ca4b1fda80a69535c23d1
Johannes <johannes@macbook-jf.fritz.box>
2024-11-03 17:16:58 +0100

    created colemak layer and added led indicator corrected keymap errors

--- keyboards/sofle_choc_arctic/keymaps/home_row_layers/keymap.c
+++ keyboards/sofle_choc_arctic/keymaps/home_row_layers/keymap.c
@@ -22,6 +22,8 @@
 
 enum custom_keycodes {
     SWITCH_OS = SAFE_RANGE,
+    MA_WRDR,
+    MA_WRDL
 };
 
 bool os_mode = OS_PC;
@@ -47,27 +49,27 @@ bool os_mode = OS_PC;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-        XXXXXXX , DE_1 , DE_2       , DE_3      , DE_4         , DE_5 ,                        DE_6   , DE_7        , DE_8       , DE_9       , DE_0    , XXXXXXX,
-        KC_ESC  , DE_Q , DE_W       , DE_E      , DE_R         , DE_T ,                        DE_Y   , DE_U        , DE_I       , DE_O       , DE_P    , XXXXXXX,
-        KC_LSFT , DE_A , LT(2,DE_S) , LT(1,DE_D), LSFT_T(DE_F) , DE_G ,                        DE_H , LSFT_T(DE_J), LT(3,DE_K) , LT(4,DE_L)     , DE_SCLN , KC_RSFT,
-        CW_TOGG , DE_Z , DE_X       , DE_C      , LT(5,DE_V)         , DE_B ,   KC_MUTE,    KC_MPLY, LT(5,DE_N)   , DE_M        , DE_COMM    , DE_DOT     , DE_QUOT , XXXXXXX,
-        SWITCH_OS , DE_M  , LCTL_T(KC_BSPC) ,LALT_T(KC_ENT) , LGUI_T(KC_TAB) ,                    KC_RGUI, RALT_T(KC_SPC), RCTL_T(KC_DEL) , XXXXXXX , XXXXXXX
+        XXXXXXX , DE_1       , DE_2       , DE_3       , DE_4         , DE_5 ,                        DE_6 , DE_7           , DE_8       , DE_9       , DE_0    , XXXXXXX,
+        KC_ESC  , DE_Q       , DE_W       , DE_E       , DE_R         , DE_T ,                        DE_Y , DE_U           , DE_I       , DE_O       , DE_P    , XXXXXXX,
+        CW_TOGG , LT(6,DE_A) , LT(4,DE_S) , LSFT_T(DE_D) , LT(2,DE_F) , DE_G ,                        DE_H , LT(3,DE_J)   , LSFT_T(DE_K) , LT(5,DE_L) , LT(7,DE_PLUS) , CW_TOGG,
+        XXXXXXX , DE_Z       , DE_X       , DE_C       , DE_V         , DE_B ,   KC_MUTE,    KC_MPLY, DE_N , DE_M , DE_COMM , DE_DOT     , DE_MINS    , XXXXXXX,
+        SWITCH_OS , TG(1) , LCTL_T(KC_BSPC) ,LALT_T(KC_ENT) , LGUI_T(KC_TAB) ,                    KC_RGUI, RALT_T(KC_SPC) , RCTL_T(KC_DEL) , XXXXXXX , XXXXXXX
     ),
 
 	[1] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                      _______, _______       , _______ , _______        , _______, _______,
-        _______, _______, _______, _______, _______, _______,                      _______ , LALT(KC_LEFT) , KC_UP   , LALT(KC_RIGHT) , KC_PSCR, _______,
-        _______, _______, _______, _______, _______, _______,                      KC_HOME, KC_LEFT       , KC_DOWN , KC_RIGHT       , KC_END , _______,
-        _______, _______, _______, _______, _______, _______, _______,    _______, _______, LCTL(KC_LEFT) , _______ , LCTL(KC_RIGHT) , _______, _______,
-             _______, _______, _______, _______, _______,                             _______ , _______, _______, _______, _______
+        XXXXXXX , DE_1       , DE_2       , DE_3       , DE_4         , DE_5 ,                        DE_6 , DE_7           , DE_8       , DE_9       , DE_0    , XXXXXXX,
+        KC_ESC  , DE_Q       , DE_W       , DE_F       , DE_P         , DE_G ,                        DE_J , DE_L           , DE_U       , DE_Y       , DE_PLUS    , XXXXXXX,
+        CW_TOGG , LT(6,DE_A) , LT(4,DE_R) , LSFT_T(DE_S) , LT(2,DE_T) , DE_D ,                        DE_H , LT(3,DE_N)   , LSFT_T(DE_E) , LT(5,DE_I) , LT(7,DE_O) , CW_TOGG,
+        XXXXXXX , DE_Z       , DE_X       , DE_C       , DE_V         , DE_B ,   KC_MUTE,    KC_MPLY, DE_K , DE_M , DE_COMM , DE_DOT     , DE_MINS    , XXXXXXX,
+        SWITCH_OS , _______, LCTL_T(KC_BSPC) ,LALT_T(KC_ENT) , LGUI_T(KC_TAB) ,                    KC_RGUI, RALT_T(KC_SPC) , RCTL_T(KC_DEL) , XXXXXXX , XXXXXXX
     ),
-
-	[2] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                       _______, _______, _______, _______, _______ , _______,
-        _______, _______, _______, _______, _______, _______,                       DE_MINS, DE_7   , DE_8   , DE_9   , DE_SLSH , _______,
-        _______, _______, _______, _______, _______, _______,                       DE_PLUS, DE_4   , DE_5   , DE_6   , DE_ASTR , _______,
-        _______, _______, _______, _______, _______, _______, _______,     _______, DE_0   , DE_1   , DE_2   , DE_3   , DE_COMM , _______,
-          _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______
+    
+    [2] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                      _______, _______  , _______ , _______  , _______, _______,
+        _______, _______, _______, _______, _______, _______,                      KC_PSCR , MA_WRDL , KC_UP   , MA_WRDR  , _______, _______,
+        _______, _______, _______, _______, _______, _______,                      KC_HOME, KC_LEFT  , KC_DOWN , KC_RIGHT , KC_END , _______,
+        _______, _______, _______, _______, _______, _______, _______,    _______, KC_MPRV ,  KC_VOLD, KC_MPLY , KC_VOLU  , KC_MNXT, _______,
+             _______, _______, _______, _______, _______,                             _______ , _______, _______, _______, _______
     ),
 
     [3] = LAYOUT(
@@ -78,21 +80,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              _______, _______, _______, _______, _______,                                  _______ , _______, _______, _______, _______
     ),
 
-    [4] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
-        _______, _______, DE_HASH, DE_CIRC, DE_QUES, _______,                    _______, _______, _______, _______, _______, _______,
-        _______, _______, DE_AT  , DE_PERC, DE_EXLM, _______,                    _______, _______, _______, _______, _______, _______,
-        _______, _______, DE_TILD, DE_DLR , DE_AMPR, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
+	[4] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                       _______, _______, _______, _______, _______ , _______,
+        _______, _______, _______, _______, _______, _______,                       DE_MINS, DE_7   , DE_8   , DE_9   , DE_SLSH , _______,
+        _______, _______, _______, _______, _______, _______,                       DE_PLUS, DE_4   , DE_5   , DE_6   , DE_ASTR , _______,
+        _______, _______, _______, _______, _______, _______, _______,     _______, DE_0   , DE_1   , DE_2   , DE_3   , DE_DOT  , _______,
           _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______
     ),
 
     [5] = LAYOUT(
-        _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                    KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11  ,
+        _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
+        _______, DE_QUOT, DE_HASH, DE_TILD, DE_QUES, DE_RABK ,                    _______, _______, _______, _______, _______, _______,
+        _______, DE_DQUO, DE_AT  , DE_PERC, DE_EXLM, DE_LABK,                    _______, _______, _______, _______, _______, _______,
+        _______, DE_CIRC, DE_DEG, DE_DLR , DE_AMPR, DE_ACUT, _______,     _______, _______, _______, _______, _______, _______, _______,
+          _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______
+    ),
+
+    [6] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______ ,
         _______, _______, _______, _______, _______, _______,                    _______, DE_UDIA, _______, DE_ODIA, _______, KC_F12  ,
+        _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
+          _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______
+    ),
+
+    [7] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______  ,
         _______, DE_ADIA, DE_SS  , _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
           _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______
     )
+
 };
 
 #if defined(ENCODER_MAP_ENABLE)
@@ -103,6 +122,8 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [3] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
     [4] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
     [5] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
+    [6] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
+    [7] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
 };
 #endif
 
@@ -116,35 +137,61 @@ void keyboard_post_init_user(void) {
 }
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    if (get_highest_layer(layer_state) > 0) {
-        uint8_t layer = get_highest_layer(layer_state);
+    uint8_t layer = get_highest_layer(layer_state);
+
+    if (layer > 1) {
+        RGB rgb = hsv_to_rgb((HSV){128, 255, rgb_matrix_get_val()});
         for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
             for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
                 uint8_t index = g_led_config.matrix_co[row][col];
-
                 if (index >= led_min && index < led_max && index != NO_LED &&
                     keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
-                    rgb_matrix_set_color(index, RGB_TEAL);
+                    rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
                 }
             }
         }
     }
 
     if (is_caps_word_on() == true) {
-        rgb_matrix_set_color(8, RGB_RED);
-        rgb_matrix_set_color(50, RGB_RED);
+        rgb_matrix_set_color(26, RGB_RED);
     }
 
     if (os_mode == OS_PC) {
-        rgb_matrix_set_color(16, RGB_WHITE);
+        RGB rgb = hsv_to_rgb((HSV){0,0,rgb_matrix_get_val()}); //white
+        rgb_matrix_set_color(16, rgb.r, rgb.g, rgb.b);
     }
-    else {
-        rgb_matrix_set_color(16, RGB_CORAL);
+    if (os_mode == OS_MAC) {
+        RGB rgb = hsv_to_rgb((HSV){15, 255, rgb_matrix_get_val()}); // dark orange
+        rgb_matrix_set_color(16, rgb.r, rgb.g, rgb.b);
+    }
+
+    if (layer == 1) {
+        RGB rgb = hsv_to_rgb((HSV){0,0,rgb_matrix_get_val()}); //white
+        rgb_matrix_set_color(15, rgb.r, rgb.g, rgb.b);
     }
 
     return false;
 }
 
+
+bool process_detected_host_os_kb(os_variant_t detected_os) {
+    if (!process_detected_host_os_user(detected_os)) {
+        return false;
+    }
+    switch (detected_os) {
+        case OS_MACOS:
+        case OS_IOS:
+            os_mode = OS_MAC;
+            break;
+        case OS_WINDOWS:
+        case OS_LINUX:
+        case OS_UNSURE:
+            os_mode = OS_PC;
+            break;
+    }
+    
+    return true;
+}
 
 // ############## MACROS ################
 
@@ -159,6 +206,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 os_mode = OS_PC;
             }
         } 
+        return true;
+    case MA_WRDR:
+        if (record->event.pressed){
+            if(os_mode == OS_PC) tap_code16(LCTL(KC_RIGHT));
+            else tap_code16(LALT(KC_RIGHT));
+        }
+        return true;
+    case MA_WRDL:
+        if (record->event.pressed) {
+            if(os_mode == OS_PC) tap_code16(LCTL(KC_LEFT));
+            else tap_code16(LALT(KC_LEFT));
+        }
         return true;
     }
 
